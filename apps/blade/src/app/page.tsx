92347968f78081f07824fc2f341bcc0e727bac09
powdermilkjuno <sg09262004@gmail.com>
2025-02-20 01:38:19 -0500

    pnpm dev running and showing very barebones page, will add color and animation

--- apps/blade/src/app/page.tsx
+++ apps/blade/src/app/page.tsx
@@ -4,7 +4,7 @@ import { Auth } from "./_components/auth-showcase";
 export default function HomePage() {
   return (
     <HydrateClient>
-      <main className="container h-screen py-16">
+      <main className="container h-screen py-16 theme-container">
         <Auth />
       </main>
     </HydrateClient>
