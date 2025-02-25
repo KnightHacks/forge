041e0fdb193a14a89eff8071efa604c6eab2e504
Mila Knickerbocker <mi959525@ucf.edu>
2025-02-25 16:46:11 -0500

    Update Page.tsx

--- apps/blade/src/app/mila/Page.tsx
+++ apps/blade/src/app/mila/Page.tsx
@@ -1,1 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mila Knickerbocker</title>
+    <link rel="stylesheet" href="styles.css" />
+  </head>
+
+<body>
+    <!-- Navbar Section -->
+    <nav class="navbar">
+      <div class="navbar__container">
+        <a href="/" id="navbar__logo">MILA KNICKERBOCKER</a>
+        <div class="navbar__toggle" id="mobile-menu">
+          <span class="bar"></span> <span class="bar"></span>
+          <span class="bar"></span>
+        </div>
+        <ul class="navbar__menu">
+          <li class="navbar__item">
+            <a href="/" class="navbar__links">Home</a>
+          </li>
+          <li class="navbar__item">
+            <a href="/tech.html" class="navbar__links" target="_blank">Resume</a>
+          </li>
+          <li class="navbar__item">
+            <a href="https://www.linkedin.com/in/mila-knickerbocker-601312292/" target = "_blank" class="navbar__links">LinkedIn</a>
+          </li>
+          <li class="navbar__item">
+              <a href="#div_id" class="navbar__links">Projects</a>
+          </li>
+          <li class="navbar__btn"><a href="mailto:knickmila@gmail.com" target="_blank" class="button">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+
+  <p> 
+    <hr>
+  </p>
+
+    <!-- Front Page -->
+    <div class="front">
+      <div class="front__container">
+        <div class="front__content">
+          <h1>MILA KNICKERBOCKER</h1>
+          <h2> Software Engineer </h2>
+        </div>
+      </div>
+    </div>
+
+    <!-- About Me Section -->
+    <div class="main">
+      <div class="main__container">
+        <div class="main__content">
+          <h1>About Me</h1>
+          <p>
+            Hello! I'm Mila Knickerbocker. I'm majoring in Computer Science with a minor in Environmental Studies at the 
+            University of Central Florida, and expect to graduate in May of 2027 with a Bachelors of Science. I have been 
+            honing my skills in computer science with programs such as Python, C and Java, and getting involved in the 
+            school’s community through organizations such as Central Florida Rowing, where I implement the skills of 
+            cooperation and discipline that I developed as a team captain and rowing coach. Prior to UCF, I held several 
+            positions, where I learned a lot about the different styles of communication. While working as a Babysitter and 
+            Pet Caretaker, I was also involved in several clubs, such as the Engineering Club, where I participated in 
+            competitions that required a strong line of communication between teammates along with the ability to 
+            problem-solve in high stress environments. I went on to win both events I competed in. I was drawn to Computer 
+            Science because of my love for math and Environmental Studies from my passion for environmentalism and 
+            sustainability. Thank you for taking the time to learn about me. I look forward to connecting with you!
+          </p>
+          <button class="main__btn"><a href="mailto:knickmila@gmail.com">Contact Me</a></button>
+        </div>
+        <div class="main__img--container">
+          <img id="main__img" src="images/ucf.jpg" />
+        </div>
+      </div>
+    </div>
+    
+    <!-- Projects Section -->
+    <div id="div_id"></div>
+    <div class="projects">
+      <h1> Projects </h1>
+      <div class="projects__container">
+        <div class="projects__card">
+          <h2> Machine Learning Development for Functional Materials Discovery </h2>
+          <p> Developed a state-of-the-art material descriptor for machine learning in computational materials science.
+              Performed first-principle calculations on high-performance computers for simple material systems.
+          </p>
+        </div>
+        <div class="projects__card">
+          <h2> Hillsborough County Science Bowl </h2>
+          <p>
+            Designed a mechanism that could lower an object expeditiously without breaking it. 
+            1st place individually and overall. 
+          </p>
+        </div>
+        <div class="projects__card">
+          <h2> Science Olympiad Regional Competition </h2>
+          <p>
+            Constructed an unknown object swiftly based on instructions from a teammate.
+            1st place individually and 6th place overall
+          </p>
+        </div>
+      </div>
+    </div>
+
+    <p>
+      <hr>
+    </p>
+
+    <!-- Footer Section -->
+    <div class="footer__title">
+      <h1>Contact Me</h1>
+    </div>
+
+    <p>
+      <hr>
+    </p>
+
+    <div class="footer">
+      <div class="footer__container">
+        <div class="footer__card">
+          <a href="https://www.instagram.com/_mila.elle_/" target="_blank">
+            <button></button>
+          </a>
+        </div>
+        <div class="footer__card">
+          <a href="https://www.linkedin.com/in/mila-knickerbocker-601312292/" target="_blank">
+            <button></button>
+          </a>
+        </div>
+        <div class="footer__card">
+          <a href="https://github.com/milaeknick" target="_blank">
+            <button></button>
+          </a>
+        </div>
+        <div class="footer__card">
+          <a href="mailto:knickmila@gmail.com" target="_blank">
+            <button></button>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="footer__copyright">
+      <p>@ 2025 by Mila Knickerbocker </p>
+    </div>
+    <script src="app.js"></script>
+  </body>
+</html>
 
