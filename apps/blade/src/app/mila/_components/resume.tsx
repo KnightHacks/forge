fd7b0c0256dcfcd90f3c44971e33174cf50f0ff6
Mila Knickerbocker <mi959525@ucf.edu>
2025-02-25 16:47:31 -0500

    Update resume.tsx

--- apps/blade/src/app/mila/_components/resume.tsx
+++ apps/blade/src/app/mila/_components/resume.tsx
@@ -1,1 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mila Knickerbocker</title>
+    <link rel="stylesheet" href="styles.css" />
+  </head>
+
+  <body>
+    
+    <!-- Navbar Section -->
+    <nav class="navbar">
+      <div class="navbar__container">
+        <a href="/" id="navbar__logo">MILA KNICKERBOCKER</a>
+        <div class="navbar__toggle" id="mobile-menu">
+          <span class="bar"></span> <span class="bar"></span>
+          <span class="bar"></span>
+        </div>
+        <ul class="navbar__menu">
+          <li class="navbar__item">
+            <a href="/" class="navbar__links">Home</a>
+          </li>
+          <li class="navbar__item">
+            <a href="/tech.html" class="navbar__links">Resume</a>
+          </li>
+          <li class="navbar__item">
+            <a href="https://www.linkedin.com/in/mila-knickerbocker-601312292/" target = "_blank" class="navbar__links">LinkedIn</a>
+          </li>
+          <li class="navbar__item">
+              <a href="/" class="navbar__links">Projects</a>
+          </li>
+          <li class="navbar__btn"><a href="mailto:knickmila@gmail.com" target="_blank" class="button">Contact</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <p> 
+      <hr>
+    </p>  
+
+    <!-- Resume Section -->
+    <div class="resume">
+      <div class="resume__container">
+        <div class="resume__content">
+          <h1>Resume</h1>
+        </div>
+        <div class="main__img-container">
+          <img id="main__img" src="images/resume.jpg" />
+        </div>
+      </div>
+    </div>
+
+    <p>
+      <hr>
+    </p>
+
+    <!-- Footer Section -->
+    <div class="footer__title">
+      <h1>Contact Me</h1>
+    </div>
+
+    <p>
+      <hr>
+    </p>
+
+    <div class="footer">
+      <div class="footer__container">
+        <div class="footer__card">
+          <a href="https://www.instagram.com/_mila.elle_/" target="_blank">
+            <button></button>
+          </a>
+        </div>
+        <div class="footer__card">
+          <a href="https://www.linkedin.com/in/mila-knickerbocker-601312292/" target="_blank">
+            <button></button>
+          </a>
+        </div>
+        <div class="footer__card">
+          <a href="https://github.com/milaeknick" target="_blank">
+            <button></button>
+          </a>
+        </div>
+        <div class="footer__card">
+          <a href="mailto:knickmila@gmail.com" target="_blank">
+            <button></button>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="footer__copyright">
+      <p>@ 2025 by Mila Knickerbocker </p>
+    </div>
+
+    <script src="app.js"></script>
+  </body>
+</html>
 
