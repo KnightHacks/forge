92eb1d545edae231a0535e2c638c8a80ef07cb0d
Josh Sawyer <jawshs08@gmail.com>
2025-02-27 03:21:06 -0500

    Added important links and made general additions to application page

--- apps/blade/src/app/JoshSawyer/page.tsx
+++ apps/blade/src/app/JoshSawyer/page.tsx
@@ -1,15 +1,54 @@
-import pmo from '/images/pmo.png';
+import Link from 'next/link';
 
 function Application() {
     return (
         <div className="Application">
-            <header className="Introduction">
-                <p>
-                    Hello vro...
-                </p>
-		<img src={pmo} className="Peeling-My-Orange" alt="pmo" />
-            </header>
-        </div>
+    		<header className="Introduction">
+        		<p>
+        			Hello vro...
+        		</p>
+        		<img src="pmo.png" className="Peeling-My-Orange" alt="pmo" />
+    		</header>
+
+    		<div>
+        		<p>
+        		    Links to my:
+        		</p>
+
+        		<Link href="https://www.linkedin.com/in/josh-sawyer-b765ab255">
+        		    LinkedIn
+        		</Link>
+				<br/>
+
+        		<Link href="https://github.com/buhgowsh">
+        		    GitHub
+        		</Link>
+				<br/>
+
+				<Link href="https://drive.google.com/file/d/1WYrZD0DD3eDpI2cIJDFu9Ms56QfYshLm/view?usp=drive_link">
+					Resume
+				</Link>
+				<br/>
+
+				<Link href="https://drive.google.com/file/d/1F7KvkwQGtMxDy-lU084TjlIYsAum7fKG/view?usp=drive_link">
+					Presentation
+				</Link>
+				<br/>
+
+        		<Link href="/JoshSawyer/reasons">
+        		    Reasons I want to join! :D
+        		</Link>
+    		</div>
+
+			<div>
+				<p>
+					I'm Josh! (buhgowsh in the discord) I'm a sophomore looking to get more involved with Knight Hacks and gain experience that I can show off on my resume.
+					I've known about JavaScript and TypeScript for a litle while, but I've never actually sat down and tried to learn it until now.
+					This application was my first experience with HTML, CSS, and TypeScript React and regardless of if I make the cut or not this has been a great learning experience!
+					Especially since I'm in POOSD currently and I'm on frontend for the large project, so getting started here will go a long way both potentially for the Dev Team and for my POOSD group.
+				</p>
+			</div>
+		</div>
     );
 }
 
