9fc7599e24b4dea171df6daa2a72b7880956a680
Suprnova <nova@suprnova.dev>
2025-08-27 21:28:51 -0400

    styling refactor, supplemental goose, a11y stuff

--- apps/blade/src/app/henry/page.tsx
+++ apps/blade/src/app/henry/page.tsx
@@ -6,44 +6,60 @@ import {
 
 export default function HenryDevApplicationPage() {
   return (
-    <main className="mx-auto mb-24 flex h-screen w-full flex-col items-center justify-around gap-8 p-8 xl:flex-row">
+    <main className="mx-auto flex h-screen w-full flex-col items-center justify-around gap-8 p-8 text-center xl:flex-row">
       <div id="intro" className="h-xl flex flex-col gap-2">
-        <h1 className="text-center text-7xl 2xl:text-left">
-          hi. i'm henry <b className="text-gray-600">██████</b>
+        <h1 className="text-7xl 2xl:text-left">
+          hi. i'm henry <strong className="text-gray-600">██████</strong>
         </h1>
-        <h2 className="text-center text-4xl 2xl:text-left">
+        <h2 className="text-4xl 2xl:text-left">
           but i also go by{" "}
           <span className="text-6xl text-yellow-400">nova</span>.
         </h2>
-        <sub className="text-center text-xl text-gray-400 2xl:text-left">
+        <sub className="text-xl text-gray-400 2xl:text-left">
           (no full name, i got opps)
         </sub>
         <div
           id="socials"
           className="flex flex-row items-stretch justify-around py-4"
         >
-          <a href="javascript:alert('@suprnova')">
+          <a
+            href="https://discord.com/users/219941863950385152"
+            aria-label="Discord"
+            target="_blank"
+          >
             <DiscordLogoIcon width="50px" height="50px" />
           </a>
-          <a href="https://github.com/Suprnova" target="_blank">
+          <a
+            href="https://github.com/Suprnova"
+            aria-label="GitHub"
+            target="_blank"
+          >
             <GitHubLogoIcon width="50px" height="50px" />
           </a>
-          <a href="https://suprnova.dev" target="_blank">
+          <a
+            href="https://suprnova.dev"
+            aria-label="Personal Website"
+            target="_blank"
+          >
             <GlobeIcon width="50px" height="50px" />
           </a>
         </div>
-        <p className="text-center text-xl">
+        <p className="text-xl">
           please get in touch with{" "}
           <strong className="text-purple-600 dark:text-purple-300">
             @suprnova
           </strong>{" "}
           on discord or{" "}
-          <a className="text-purple-400" href="mailto:nova@suprnova.dev">
+          <a
+            className="text-purple-400"
+            aria-label="Email Address"
+            href="mailto:nova@suprnova.dev"
+          >
             nova@suprnova.dev
           </a>{" "}
           for a resume link
         </p>
-        <sub className="text-center text-sm">(privacy reasons)</sub>
+        <sub className="text-sm">(privacy reasons)</sub>
       </div>
 
       <div
@@ -51,14 +67,20 @@ export default function HenryDevApplicationPage() {
         className="flex flex-col items-center justify-center gap-4 xl:justify-normal"
       >
         <img
-          className="max-w-[400px]"
           alt="illustration of henry"
+          className="max-w-[400px]"
           src="../nova_portrait.jpg"
         />
         <strong className="text-center">
           i don't have a good portrait, so here's old art of myself instead
           (liberties taken)
         </strong>
+
+        <img
+          alt="a silly goose"
+          className="m-8 h-8 duration-300 hover:scale-[4]"
+          src="../goose.gif"
+        />
       </div>
     </main>
   );
