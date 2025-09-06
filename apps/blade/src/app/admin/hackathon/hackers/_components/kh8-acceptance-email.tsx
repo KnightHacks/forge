b1daf1e94b167852a33579b5847149e41e941fff
Luke Cullen <75819270+retromonos@users.noreply.github.com>
2025-09-06 16:05:29 -0400

    initial draft of acceptance email

--- apps/blade/src/app/admin/hackathon/hackers/_components/kh8-acceptance-email.tsx
+++ apps/blade/src/app/admin/hackathon/hackers/_components/kh8-acceptance-email.tsx
@@ -7,6 +7,7 @@ import {
   Preview,
   Section,
   Tailwind,
+  Link,
   Text,
 } from "@react-email/components";
 
@@ -31,11 +32,12 @@ export const KH8AcceptanceEmail = ({ name }: AcceptanceEmailProps) => {
           },
         }}
       >
-        <Body className="m-0 bg-[#ECECEC] p-0 font-sans">
+        <Body className="bg-white m-0 p-0 font-sans"
+        style={{backgroundColor: "#ffffff"}}>
           <Preview>{previewText}</Preview>
 
-          <Container className="mx-auto max-w-[600px] p-0">
-            <Section className="my-1 p-0">
+          <Container className="mx-auto max-w-[700px] p-0">
+            <Section className="p-0">
               <table
                 role="presentation"
                 width="100%"
@@ -44,386 +46,78 @@ export const KH8AcceptanceEmail = ({ name }: AcceptanceEmailProps) => {
                 border={0}
               >
                 <tr>
-                  <td align="center">
-                    <Img
-                      src="https://i.imgur.com/lpTVNl7.png"
-                      width={700}
-                      height="auto"
-                      alt="Divider"
-                      style={{
-                        maxWidth: "50%",
-                        height: "auto",
-                        margin: "0 auto",
-                      }}
-                    />
-                  </td>
-                </tr>
-              </table>
-            </Section>
-
-            <Section className="z-10 mx-auto w-full p-0">
-              <table
-                role="presentation"
-                width="100%"
-                cellPadding={0}
-                cellSpacing={0}
-                border={0}
-              >
-                <tr>
-                  <td
+                  <td 
                     // @ts-expect-error td is tripping
-                    background="https://i.imgur.com/XlGEJ11.png"
+                    background="https://i.imgur.com/OdRQ4Xy.jpeg"
+                    align="right" 
                     width="700"
-                    height="200"
-                    align="left"
-                    valign="middle"
+                    height="380"
+                    valign="top"
                     style={{
-                      backgroundSize: "cover",
-                      backgroundPosition: "center",
+                      backgroundSize: "700px 380px"
                     }}
-                  >
-                    <Text className="mb-6 p-[22px] text-[28px] font-bold leading-tight text-[#070708]">
-                      &nbps;<span className="text-[#4075B7]">Congrats,</span>
-                      <br />
-                      &nbps;
-                      <span className="text-[32px] font-bold text-[#C04B3D]">
-                        {name}!
-                      </span>
+                    className="rounded-t-md relative"
+                    >
+                      <Text className="mt-12 mb-6 p-[22px] text-[20px] leading-tight text-[#C04B3D]">
+                        <span className="text-[#C04B3D] mb-2 font-normal">Congratulations,</span>
+                        <br />
+                        <span className="text-[20px] font-bold text-[#C04B3D]">
+                          {name}
+                        </span>
+                      </Text>
+                      <Img src="https://i.imgur.com/aw3sWEV.png" className="w-[500px] h-[200px] absolute"/>
+                  </td>
+                </tr>
+              </table>
+            </Section>
+            <Section className="p-0">
+              <table
+                role="presentation"
+                width="100%"
+                cellPadding={0}
+                cellSpacing={0}
+                border={0}
+                bgcolor="#F7F0C6"
+                className="p-[24px]"
+              >
+                <tr>
+                  <td>
+                      <hr/>
+                      <Text className="text-[32px] leading-tight text-black font-bold text-center">
+                        October 24th - 26th, 2025
+                        <br/>
+                        <span className="text-[#4075b7] text-[24px]/8">
+                          University of Central Florida
+                          <br/>
+                          Engineering I, Room 102
+                        </span>
+                      </Text>
+                      <hr/>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                      <Text className="text-[36px] leading-tight text-[#C04B3D] font-bold italic underline">
+                        NEXT STEPS...
+                      </Text>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-4">
+                      <Text className="text-[20px] leading-tight text-[#4075b7] font-bold">
+                        1) Confirm your attendance!
+                      </Text>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-4">
+                    <Text className="text-[20px] leading-tight text-black">
+                      While you have been accepted to <span className="font-bold text-[#C04B3D]">KnightHacks VIII</span>, you have <span className="font-bold">one last step</span>.
+                      <br/><br/>
+                      Confirm that you are able to attend the event using Blade, at your <Link href={"https://blade.knighthacks.org/dashboard"} className="text-[#4075b7] underline">
+                        hacker dashboard.
+                      </Link> If you do not confirm by the deadline, you may <span className="text-[#C04B3D]">lose your spot</span>, so do this <span className="font-bold">ASAP!</span>
                     </Text>
-                  </td>
-                </tr>
-              </table>
-            </Section>
-
-            <Section className="p-0 text-center">
-              <Img
-                src="https://i.imgur.com/qLVlama.png"
-                width={600}
-                alt="KnightHacks Banner"
-                className="mt-[-50px] h-auto w-full"
-              />
-            </Section>
-
-            <Section className="my-10 p-0">
-              <table
-                role="presentation"
-                width="100%"
-                cellPadding={0}
-                cellSpacing={0}
-                border={0}
-              >
-                <tr>
-                  <td align="center">
-                    <Img
-                      src="https://i.imgur.com/RSl6O6h.png"
-                      width={700}
-                      height="auto"
-                      alt="Divider"
-                      style={{
-                        maxWidth: "100%",
-                        height: "auto",
-                        margin: "0 auto",
-                      }}
-                    />
-                  </td>
-                </tr>
-              </table>
-            </Section>
-
-            <Section className="mx-auto w-full p-0">
-              <table
-                role="presentation"
-                width="100%"
-                cellPadding={0}
-                cellSpacing={0}
-                border={0}
-              >
-                <tr>
-                  <td align="center" style={{ padding: "20px 0" }}>
-                    <Text
-                      className="text-[40px] font-normal leading-[46px] tracking-[0.01em]"
-                      style={{ margin: 0 }}
-                    >
-                      1. CONFIRM YOUR{" "}
-                      <span className="font-bold text-[#C04B3D]">SPOT!</span>
-                    </Text>
-                  </td>
-                </tr>
-                <tr>
-                  <td align="center" style={{ padding: "10px 0" }}>
-                    <Text
-                      className="text-[20px] font-normal leading-[23px] tracking-[0.01em]"
-                      style={{ margin: 0 }}
-                    >
-                      (Spots are filling up quickly confirm ASAP!)
-                    </Text>
-                  </td>
-                </tr>
-                <tr>
-                  <td align="center" style={{ padding: "20px 0" }}>
-                    <Img
-                      src="https://i.imgur.com/h8b8QMm.png"
-                      width={500}
-                      height="auto"
-                      alt="Confirm Spot Graphic"
-                      style={{ maxWidth: "100%", height: "auto" }}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td align="center" style={{ padding: "10px 0" }}>
-                    <a href={`https://blade.knighthacks.org/dashboard`}>
-                      <Img
-                        src="https://i.imgur.com/Lxts1BH.png"
-                        width={280}
-                        height="auto"
-                        alt="Confirm Button"
-                        style={{ height: "auto" }}
-                      />
-                    </a>
-                  </td>
-                </tr>
-              </table>
-            </Section>
-
-            <Section className="mx-auto w-full p-0">
-              <table
-                role="presentation"
-                width="100%"
-                cellPadding={0}
-                cellSpacing={0}
-                border={0}
-              >
-                <tr>
-                  <td align="center" style={{ padding: "20px 0" }}>
-                    <Text
-                      className="text-[40px] font-normal leading-[46px] tracking-[0.01em]"
-                      style={{ margin: 0 }}
-                    >
-                      2. JOIN OUR{" "}
-                      <span className="font-bold text-[#4075B7]">DISCORD!</span>
-                    </Text>
-                  </td>
-                </tr>
-                <tr>
-                  <td align="center" style={{ padding: "10px 0" }}>
-                    <Text
-                      className="text-[20px] font-normal leading-[23px] tracking-[0.01em]"
-                      style={{ margin: 0 }}
-                    >
-                      (Required by October 23rd)
-                    </Text>
-                  </td>
-                </tr>
-                <tr>
-                  <td align="center" style={{ padding: "20px 0" }}>
-                    <Img
-                      src="https://i.imgur.com/j67gvlZ.png"
-                      width={500}
-                      height="auto"
-                      alt="Discord Graphic"
-                      style={{ maxWidth: "100%", height: "auto" }}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td align="center" style={{ padding: "10px 0" }}>
-                    <a href={"https://discord.com/invite/Kv5g9vf"}>
-                      <Img
-                        src="https://i.imgur.com/CuX6BqL.png"
-                        width={280}
-                        height="auto"
-                        alt="Join Discord Button"
-                        style={{ height: "auto" }}
-                      />
-                    </a>
-                  </td>
-                </tr>
-              </table>
-            </Section>
-
-            <Section className="mx-auto w-full p-0">
-              <table
-                role="presentation"
-                width="100%"
-                cellPadding={0}
-                cellSpacing={0}
-                border={0}
-              >
-                <tr>
-                  <td align="center" style={{ padding: "20px 0" }}>
-                    <Text
-                      className="text-[40px] font-normal leading-[46px] tracking-[0.01em]"
-                      style={{ margin: 0 }}
-                    >
-                      3. SPREAD THE
-                      <br />
-                      <span className="text-5xl font-bold text-[#C04B3D]">
-                        EXCITEMENT!
-                      </span>
-                    </Text>
-                  </td>
-                </tr>
-                <tr>
-                  <td align="center" style={{ padding: "10px 0" }}>
-                    <Img
-                      src="https://i.imgur.com/loDXiue.png"
-                      width={500}
-                      height="auto"
-                      alt="Poster Graphic"
-                      style={{ maxWidth: "100%", height: "auto" }}
-                    />
-                  </td>
-                </tr>
-                <tr>
-                  <td align="center" style={{ padding: "5px 0" }}>
-                    <table
-                      role="presentation"
-                      width="100%"
-                      cellPadding={0}
-                      cellSpacing={0}
-                      border={0}
-                    >
-                      <td align="center">
-                        <Img
-                          src="https://i.imgur.com/rlx2A55.png"
-                          width={150}
-                          height="auto"
-                          alt="Poster No Text"
-                          style={{ height: "auto" }}
-                        />
-                      </td>
-                      <td align="center" style={{ padding: "5px 0" }}>
-                        <Img
-                          src="https://i.imgur.com/5TuuXe0.png"
-                          width={150}
-                          height="auto"
-                          alt="Website Background"
-                          style={{ height: "auto" }}
-                        />
-                      </td>
-                      <td align="center" style={{ padding: "5px 0" }}>
-                        <Img
-                          src="https://i.imgur.com/6X2ux1W.png"
-                          width={150}
-                          height="auto"
-                          alt="Poster No Mascots"
-                          style={{ height: "auto" }}
-                        />
-                      </td>
-                    </table>
-                  </td>
-                </tr>
-              </table>
-            </Section>
-
-            <Section className="my-10 p-0">
-              <table
-                role="presentation"
-                width="100%"
-                cellPadding={0}
-                cellSpacing={0}
-                border={0}
-              >
-                <tr>
-                  <td align="center">
-                    <Img
-                      src="https://i.imgur.com/zv7o3rj.png"
-                      width={700}
-                      height="auto"
-                      alt="Divider"
-                      style={{
-                        maxWidth: "100%",
-                        height: "auto",
-                        margin: "0 auto",
-                      }}
-                    />
-                  </td>
-                </tr>
-              </table>
-            </Section>
-
-            {/* RESOURCES LINKS */}
-            <Section className="mx-auto w-full p-0">
-              <table
-                role="presentation"
-                width="100%"
-                cellPadding={0}
-                cellSpacing={0}
-                border={0}
-              >
-                <tr>
-                  <td align="center" style={{ padding: "20px 0" }}>
-                    <table
-                      role="presentation"
-                      cellPadding={0}
-                      cellSpacing={0}
-                      border={0}
-                    >
-                      <tr>
-                        <td align="center" style={{ paddingRight: "10px" }}>
-                          <a href="https://2025.knighthacks.org/">
-                            <Img
-                              src="https://i.imgur.com/csimGSU.png"
-                              width={240}
-                              height="auto"
-                              alt="Resource 1"
-                              style={{ height: "auto" }}
-                            />
-                          </a>
-                        </td>
-                        <td align="center">
-                          <a
-                            href={`https://knight-hacks.notion.site/knight-hacks-viii`}
-                          >
-                            <Img
-                              src="https://i.imgur.com/QGmXqoQ.png"
-                              width={240}
-                              height="auto"
-                              alt="Resource 2"
-                              style={{ height: "auto" }}
-                            />
-                          </a>
-                        </td>
-                      </tr>
-                    </table>
-                  </td>
-                </tr>
-                <tr>
-                  <td align="center" style={{ padding: "10px 0" }}>
-                    <Img
-                      src="https://i.imgur.com/4h4rs7S.png"
-                      width={500}
-                      height="auto"
-                      alt="Footer Image"
-                      style={{ maxWidth: "100%", height: "auto" }}
-                    />
-                  </td>
-                </tr>
-              </table>
-            </Section>
-
-            <Section className="my-1 p-0">
-              <table
-                role="presentation"
-                width="100%"
-                cellPadding={0}
-                cellSpacing={0}
-                border={0}
-              >
-                <tr>
-                  <td align="center">
-                    <Img
-                      src="https://i.imgur.com/lpTVNl7.png"
-                      width={700}
-                      height="auto"
-                      alt="Divider"
-                      style={{
-                        maxWidth: "50%",
-                        height: "auto",
-                        margin: "0 auto",
-                      }}
-                    />
                   </td>
                 </tr>
               </table>
