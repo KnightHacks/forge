e3b267d57041e6ef334534a99ba17d546b2e546f
hector1128 <hectorhoraciocordero11@gmail.com>
2025-09-14 14:57:06 -0400

    added resume/cover image | testing directory works

---
+++ apps/blade/src/app/hector-cordero/page.tsx
@@ -0,0 +1,11 @@
+
+export const metadata = {
+  title: "Hector Cordero | KnightHacks Dev Application",
+  description: "Calm luh Application, nun too crazy",
+};
+
+export default function Page() {
+  return (
+    <p>test</p>
+  );
+}
