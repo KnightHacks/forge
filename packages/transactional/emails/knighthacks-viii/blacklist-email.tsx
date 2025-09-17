d24e068a9708c3d2df3c7e548edb7ec580dcbda5
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2025-09-17 00:30:08 -0400

    Format fixes

--- packages/transactional/emails/knighthacks-viii/blacklist-email.tsx
+++ packages/transactional/emails/knighthacks-viii/blacklist-email.tsx
@@ -3,10 +3,10 @@ import {
   Container,
   Head,
   Html,
+  Link,
   Preview,
   Section,
   Tailwind,
-  Link,
   Text,
 } from "@react-email/components";
 
@@ -19,7 +19,7 @@ export const KH8BlacklistEmail = ({ name }: ConfirmationEmailProps) => {
 
   return (
     <Html>
-      <Head/>
+      <Head />
       <Tailwind
         config={{
           theme: {
@@ -31,11 +31,13 @@ export const KH8BlacklistEmail = ({ name }: ConfirmationEmailProps) => {
           },
         }}
       >
-        <Body className="bg-white m-0 p-0 font-sans"
-        style={{backgroundColor: "#ffffff"}}>
+        <Body
+          className="m-0 bg-white p-0 font-sans"
+          style={{ backgroundColor: "#ffffff" }}
+        >
           <Preview>{previewText}</Preview>
 
-          <Container className=" mx-auto max-w-[700px] p-0 bg-[#F7F0C6]">
+          <Container className="mx-auto max-w-[700px] bg-[#F7F0C6] p-0">
             <Section className="p-0">
               <table
                 role="presentation"
@@ -46,18 +48,18 @@ export const KH8BlacklistEmail = ({ name }: ConfirmationEmailProps) => {
                 bgcolor="#F7F0C6"
               >
                 <tr>
-                  <td 
+                  <td
                     // @ts-expect-error td is tripping
                     background="https://i.imgur.com/OdRQ4Xy.jpeg"
-                    align="right" 
+                    align="right"
                     width="700"
                     height="180"
                     valign="top"
                     style={{
-                      backgroundSize: "700px 365px"
+                      backgroundSize: "700px 365px",
                     }}
-                    className="rounded-t-md relative"
-                    />
+                    className="relative rounded-t-md"
+                  />
                 </tr>
               </table>
             </Section>
@@ -73,24 +75,66 @@ export const KH8BlacklistEmail = ({ name }: ConfirmationEmailProps) => {
               >
                 <tr>
                   <td className="px-4">
-                      <Text className="text-[20px] leading-tight text-black">
-                        <span className="font-bold text-[24px] text-[#C04B3D]">{name},</span><br/><br/>
-                        Unfortunately, due to poor conduct at prior Knight Hacks or MLH-partnered events, we have made the difficult decision to <span className="text-[#C04B3D] font-bold">deny you from Knight Hacks VIII. </span> 
-                        <br/><br/>
-                        You can read the <Link className="text-[#4075b7] underline" href="https://knight-hacks.notion.site/code-of-conduct">
-                        Knight Hacks Code of Conduct</Link>, as well as the <Link className="text-[#4075b7] underline" href="https://mlh.io/code-of-conduct">
-                        MLH Code of Conduct</Link>, using the links provided. For further questions, you can also email us at <Link className="text-[#4075b7] underline" href="mailto:hack@knighthacks.org">
-                        hack@knighthacks.org</Link>.
-                        <br/><br/>
-                        This decision is <span className="text-[#C04B3D] font-bold">final.</span>
-                        <br/><br/>
-                        However, you can try applying again at our next hackathon, or keep an eye on our <Link href={"https://discord.com/invite/Kv5g9vf"} className="text-[#4075b7] underline">
-                          Discord
-                        </Link> for news on other hackathons in the region.
-                        <br/><br/>
-                        Thanks for your interest,<br/>
-                        <span className="font-bold text-[#4075b7]">The Knight Hacks Team</span>
-                      </Text>
+                    <Text className="text-[20px] leading-tight text-black">
+                      <span className="text-[24px] font-bold text-[#C04B3D]">
+                        {name},
+                      </span>
+                      <br />
+                      <br />
+                      Unfortunately, due to poor conduct at prior Knight Hacks
+                      or MLH-partnered events, we have made the difficult
+                      decision to{" "}
+                      <span className="font-bold text-[#C04B3D]">
+                        deny you from Knight Hacks VIII.{" "}
+                      </span>
+                      <br />
+                      <br />
+                      You can read the{" "}
+                      <Link
+                        className="text-[#4075b7] underline"
+                        href="https://knight-hacks.notion.site/code-of-conduct"
+                      >
+                        Knight Hacks Code of Conduct
+                      </Link>
+                      , as well as the{" "}
+                      <Link
+                        className="text-[#4075b7] underline"
+                        href="https://mlh.io/code-of-conduct"
+                      >
+                        MLH Code of Conduct
+                      </Link>
+                      , using the links provided. For further questions, you can
+                      also email us at{" "}
+                      <Link
+                        className="text-[#4075b7] underline"
+                        href="mailto:hack@knighthacks.org"
+                      >
+                        hack@knighthacks.org
+                      </Link>
+                      .
+                      <br />
+                      <br />
+                      This decision is{" "}
+                      <span className="font-bold text-[#C04B3D]">final.</span>
+                      <br />
+                      <br />
+                      However, you can try applying again at our next hackathon,
+                      or keep an eye on our{" "}
+                      <Link
+                        href={"https://discord.com/invite/Kv5g9vf"}
+                        className="text-[#4075b7] underline"
+                      >
+                        Discord
+                      </Link>{" "}
+                      for news on other hackathons in the region.
+                      <br />
+                      <br />
+                      Thanks for your interest,
+                      <br />
+                      <span className="font-bold text-[#4075b7]">
+                        The Knight Hacks Team
+                      </span>
+                    </Text>
                   </td>
                 </tr>
               </table>
@@ -107,12 +151,42 @@ export const KH8BlacklistEmail = ({ name }: ConfirmationEmailProps) => {
               >
                 <tr>
                   <td className="px-4 text-center">
-                    <Link className="text-[#F7F0C6] underline mr-2" href="https://discord.com/invite/Kv5g9vf">Discord</Link>
-                    <Link className="text-[#F7F0C6] underline mr-2" href="https://www.instagram.com/knighthacks/">Instagram</Link>
-                    <Link className="text-[#F7F0C6] underline mr-2" href="https://blade.knighthacks.org/dashboard">Blade</Link>
-                    <Link className="text-[#F7F0C6] underline mr-2" href="https://knight-hacks.notion.site/knight-hacks-viii">Hacker's Guide</Link>
-                    <Link className="text-[#F7F0C6] underline mr-2" href="https://mlh.io/code-of-conduct">MLH Code of Conduct</Link>
-                    <Link className="text-[#F7F0C6] underline mr-2" href="https://knight-hacks.notion.site/code-of-conduct">Knight Hacks Code of Conduct</Link>
+                    <Link
+                      className="mr-2 text-[#F7F0C6] underline"
+                      href="https://discord.com/invite/Kv5g9vf"
+                    >
+                      Discord
+                    </Link>
+                    <Link
+                      className="mr-2 text-[#F7F0C6] underline"
+                      href="https://www.instagram.com/knighthacks/"
+                    >
+                      Instagram
+                    </Link>
+                    <Link
+                      className="mr-2 text-[#F7F0C6] underline"
+                      href="https://blade.knighthacks.org/dashboard"
+                    >
+                      Blade
+                    </Link>
+                    <Link
+                      className="mr-2 text-[#F7F0C6] underline"
+                      href="https://knight-hacks.notion.site/knight-hacks-viii"
+                    >
+                      Hacker's Guide
+                    </Link>
+                    <Link
+                      className="mr-2 text-[#F7F0C6] underline"
+                      href="https://mlh.io/code-of-conduct"
+                    >
+                      MLH Code of Conduct
+                    </Link>
+                    <Link
+                      className="mr-2 text-[#F7F0C6] underline"
+                      href="https://knight-hacks.notion.site/code-of-conduct"
+                    >
+                      Knight Hacks Code of Conduct
+                    </Link>
                     <Text className="text-[14px] leading-tight text-[#F7F0C6]">
                       Made with 💛 by the Knight Hacks team.
                     </Text>
@@ -128,6 +202,3 @@ export const KH8BlacklistEmail = ({ name }: ConfirmationEmailProps) => {
 };
 
 export default KH8BlacklistEmail;
-
-
-                
--- packages/transactional/emails/knighthacks-viii/confirmation-email.tsx
+++ packages/transactional/emails/knighthacks-viii/confirmation-email.tsx
@@ -4,10 +4,10 @@ import {
   Head,
   Html,
   Img,
+  Link,
   Preview,
   Section,
   Tailwind,
-  Link,
   Text,
 } from "@react-email/components";
 
@@ -20,7 +20,7 @@ export const KH8ConfirmationEmail = ({ name }: ConfirmationEmailProps) => {
 
   return (
     <Html>
-      <Head/>
+      <Head />
       <Tailwind
         config={{
           theme: {
@@ -32,11 +32,13 @@ export const KH8ConfirmationEmail = ({ name }: ConfirmationEmailProps) => {
           },
         }}
       >
-        <Body className="bg-white m-0 p-0 font-sans"
-        style={{backgroundColor: "#ffffff"}}>
+        <Body
+          className="m-0 bg-white p-0 font-sans"
+          style={{ backgroundColor: "#ffffff" }}
+        >
           <Preview>{previewText}</Preview>
 
-          <Container className=" mx-auto max-w-[700px] p-0 bg-[#F7F0C6]">
+          <Container className="mx-auto max-w-[700px] bg-[#F7F0C6] p-0">
             <Section className="p-0">
               <table
                 role="presentation"
@@ -47,26 +49,31 @@ export const KH8ConfirmationEmail = ({ name }: ConfirmationEmailProps) => {
                 bgcolor="#F7F0C6"
               >
                 <tr>
-                  <td 
+                  <td
                     // @ts-expect-error td is tripping
                     background="https://i.imgur.com/bYEtu1M.png"
-                    align="right" 
+                    align="right"
                     width="700"
                     height="400"
                     valign="top"
                     style={{
-                      backgroundSize: "700px 436px"
+                      backgroundSize: "700px 436px",
                     }}
-                    className="rounded-t-md relative"
-                    >
-                      <Text className="mt-12 mb-6 p-[22px] text-[22px] leading-tight shadow-md text-[#C04B3D]">
-                        <span className="text-[#C04B3D] mb-2 font-normal">Congratulations,</span>
-                        <br />
-                        <span className="text-[20px] font-bold text-[#C04B3D]">
-                          {name}
-                        </span>
-                      </Text>
-                      <Img src="https://i.imgur.com/W2DrFwX.png" className="w-[500px] h-[200px] absolute mt-[72px]"/>
+                    className="relative rounded-t-md"
+                  >
+                    <Text className="mb-6 mt-12 p-[22px] text-[22px] leading-tight text-[#C04B3D] shadow-md">
+                      <span className="mb-2 font-normal text-[#C04B3D]">
+                        Congratulations,
+                      </span>
+                      <br />
+                      <span className="text-[20px] font-bold text-[#C04B3D]">
+                        {name}
+                      </span>
+                    </Text>
+                    <Img
+                      src="https://i.imgur.com/W2DrFwX.png"
+                      className="absolute mt-[72px] h-[200px] w-[500px]"
+                    />
                   </td>
                 </tr>
               </table>
@@ -83,86 +90,138 @@ export const KH8ConfirmationEmail = ({ name }: ConfirmationEmailProps) => {
               >
                 <tr>
                   <td>
-                      <hr/>
-                      <Text className="text-[32px] leading-tight text-black font-bold text-center">
-                        October 24th - 26th, 2025
-                        <br/>
-                        <span className="text-[#4075b7] text-[24px]/8">
-                          University of Central Florida
-                          <br/>
-                          Engineering Atrium
-                        </span>
-                      </Text>
-                      <hr/>
+                    <hr />
+                    <Text className="text-center text-[32px] font-bold leading-tight text-black">
+                      October 24th - 26th, 2025
+                      <br />
+                      <span className="text-[24px]/8 text-[#4075b7]">
+                        University of Central Florida
+                        <br />
+                        Engineering Atrium
+                      </span>
+                    </Text>
+                    <hr />
                   </td>
                 </tr>
                 <tr>
                   <td className="">
-                      <Text className="text-[36px] leading-tight text-[#C04B3D] font-bold underline">
-                        Welcome to Knight Hacks VIII!
-                      </Text>
-                  </td>
-                </tr>
-                <tr>
-                  <td className="px-4">
-                      <Text className="text-[20px] leading-tight text-black">
-                        We're so happy you chose to join us on this journey, and we can't wait to have you at the event! Now that you're confirmed, <span className="font-bold text-[#4075b7]">your spot is saved</span> and you can rest easy.  
-                        However, we'd recommend doing <span className="font-bold">these 3 things</span> before you get here, so that you can maximize your enjoyment of the event:
-                      </Text>
-                  </td>
-                </tr>
-                <tr>
-                  <td className="">
-                      <Text className="text-[28px] leading-tight text-[#4075b7] font-bold">
-                        1) Build a Team!
-                      </Text>
+                    <Text className="text-[36px] font-bold leading-tight text-[#C04B3D] underline">
+                      Welcome to Knight Hacks VIII!
+                    </Text>
                   </td>
                 </tr>
                 <tr>
                   <td className="px-4">
                     <Text className="text-[20px] leading-tight text-black">
-                      While you can submit solo, everything's better with a team, and <span className="font-bold text-[#C04B3D]">Knight Hacks</span> is no different. If you don't have anyone to go with, don't worry! 
-                      We will have in-person team building at the start of the event, or you can find teammates on our <Link href={"https://discord.com/invite/Kv5g9vf"} className="text-[#4075b7] underline">
+                      We're so happy you chose to join us on this journey, and
+                      we can't wait to have you at the event! Now that you're
+                      confirmed,{" "}
+                      <span className="font-bold text-[#4075b7]">
+                        your spot is saved
+                      </span>{" "}
+                      and you can rest easy. However, we'd recommend doing{" "}
+                      <span className="font-bold">these 3 things</span> before
+                      you get here, so that you can maximize your enjoyment of
+                      the event:
+                    </Text>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="">
+                    <Text className="text-[28px] font-bold leading-tight text-[#4075b7]">
+                      1) Build a Team!
+                    </Text>
+                  </td>
+                </tr>
+                <tr>
+                  <td className="px-4">
+                    <Text className="text-[20px] leading-tight text-black">
+                      While you can submit solo, everything's better with a
+                      team, and{" "}
+                      <span className="font-bold text-[#C04B3D]">
+                        Knight Hacks
+                      </span>{" "}
+                      is no different. If you don't have anyone to go with,
+                      don't worry! We will have in-person team building at the
+                      start of the event, or you can find teammates on our{" "}
+                      <Link
+                        href={"https://discord.com/invite/Kv5g9vf"}
+                        className="text-[#4075b7] underline"
+                      >
                         Discord server!
-                      </Link> 
+                      </Link>
                     </Text>
                   </td>
                 </tr>
                 <tr>
                   <td className="">
-                      <Text className="text-[28px] leading-tight text-[#4075b7] font-bold">
-                        2) Read the Hacker's Guide!
-                      </Text>
+                    <Text className="text-[28px] font-bold leading-tight text-[#4075b7]">
+                      2) Read the Hacker's Guide!
+                    </Text>
                   </td>
                 </tr>
                 <tr>
                   <td className="px-4">
                     <Text className="text-[20px] leading-tight text-black">
-                      Your best friend during <span className="font-bold text-[#C04B3D]">Knight Hacks VIII</span> will be the <Link href={"https://knight-hacks.notion.site/knight-hacks-viii"} className="text-[#4075b7] underline">
+                      Your best friend during{" "}
+                      <span className="font-bold text-[#C04B3D]">
+                        Knight Hacks VIII
+                      </span>{" "}
+                      will be the{" "}
+                      <Link
+                        href={
+                          "https://knight-hacks.notion.site/knight-hacks-viii"
+                        }
+                        className="text-[#4075b7] underline"
+                      >
                         Hacker's Guide.
-                      </Link> Everything you need to know about the event will be posted there, like <span className="text-[#4075b7]">schedules, sponsor challenges, prizes, </span> 
-                      and <span className="text-[#C04B3D]">MORE!</span> Be sure to keep an eye on it, as the Guide will be regularly updated as we get closer to the event.
+                      </Link>{" "}
+                      Everything you need to know about the event will be posted
+                      there, like{" "}
+                      <span className="text-[#4075b7]">
+                        schedules, sponsor challenges, prizes,{" "}
+                      </span>
+                      and <span className="text-[#C04B3D]">MORE!</span> Be sure
+                      to keep an eye on it, as the Guide will be regularly
+                      updated as we get closer to the event.
                     </Text>
                   </td>
                 </tr>
                 <tr>
                   <td className="">
-                      <Text className="text-[28px] leading-tight text-[#4075b7] font-bold">
-                        3) Spread the EXCITEMENT!
-                      </Text>
+                    <Text className="text-[28px] font-bold leading-tight text-[#4075b7]">
+                      3) Spread the EXCITEMENT!
+                    </Text>
                   </td>
                 </tr>
                 <tr>
                   <td className="px-4">
                     <Text className="text-[20px] leading-tight text-black">
-                      Rally your team for <span className="font-bold text-[#C04B3D]">Knight Hacks VIII!</span> Post on your LinkedIn to show employers what you've got, or tell your friends to join the fray on Instagram! 
-                      Follow our Instagram page at <Link href={"https://www.instagram.com/knighthacks/"} className="underline text-[#4075b7]">@knighthacks</Link> for even more flyers and posts to get hyped ahead of the event, or make your own using <span className="text-[#4075b7]">#knighthacks</span>!
+                      Rally your team for{" "}
+                      <span className="font-bold text-[#C04B3D]">
+                        Knight Hacks VIII!
+                      </span>{" "}
+                      Post on your LinkedIn to show employers what you've got,
+                      or tell your friends to join the fray on Instagram! Follow
+                      our Instagram page at{" "}
+                      <Link
+                        href={"https://www.instagram.com/knighthacks/"}
+                        className="text-[#4075b7] underline"
+                      >
+                        @knighthacks
+                      </Link>{" "}
+                      for even more flyers and posts to get hyped ahead of the
+                      event, or make your own using{" "}
+                      <span className="text-[#4075b7]">#knighthacks</span>!
                     </Text>
                   </td>
                 </tr>
                 <tr>
-                  <td align="center" className="px-4 mx-auto">
-                    <Img className="w-[300px] mx-auto" src="https://i.imgur.com/loDXiue.png"/>
+                  <td align="center" className="mx-auto px-4">
+                    <Img
+                      className="mx-auto w-[300px]"
+                      src="https://i.imgur.com/loDXiue.png"
+                    />
                   </td>
                 </tr>
               </table>
@@ -179,12 +238,42 @@ export const KH8ConfirmationEmail = ({ name }: ConfirmationEmailProps) => {
               >
                 <tr>
                   <td className="px-4 text-center">
-                    <Link className="text-[#F7F0C6] underline mr-2" href="https://discord.com/invite/Kv5g9vf">Discord</Link>
-                    <Link className="text-[#F7F0C6] underline mr-2" href="https://www.instagram.com/knighthacks/">Instagram</Link>
-                    <Link className="text-[#F7F0C6] underline mr-2" href="https://blade.knighthacks.org/dashboard">Blade</Link>
-                    <Link className="text-[#F7F0C6] underline mr-2" href="https://knight-hacks.notion.site/knight-hacks-viii">Hacker's Guide</Link>
-                    <Link className="text-[#F7F0C6] underline mr-2" href="https://mlh.io/code-of-conduct">MLH Code of Conduct</Link>
-                    <Link className="text-[#F7F0C6] underline mr-2" href="https://knight-hacks.notion.site/code-of-conduct">Knight Hacks Code of Conduct</Link>
+                    <Link
+                      className="mr-2 text-[#F7F0C6] underline"
+                      href="https://discord.com/invite/Kv5g9vf"
+                    >
+                      Discord
+                    </Link>
+                    <Link
+                      className="mr-2 text-[#F7F0C6] underline"
+                      href="https://www.instagram.com/knighthacks/"
+                    >
+                      Instagram
+                    </Link>
+                    <Link
+                      className="mr-2 text-[#F7F0C6] underline"
+                      href="https://blade.knighthacks.org/dashboard"
+                    >
+                      Blade
+                    </Link>
+                    <Link
+                      className="mr-2 text-[#F7F0C6] underline"
+                      href="https://knight-hacks.notion.site/knight-hacks-viii"
+                    >
+                      Hacker's Guide
+                    </Link>
+                    <Link
+                      className="mr-2 text-[#F7F0C6] underline"
+                      href="https://mlh.io/code-of-conduct"
+                    >
+                      MLH Code of Conduct
+                    </Link>
+                    <Link
+                      className="mr-2 text-[#F7F0C6] underline"
+                      href="https://knight-hacks.notion.site/code-of-conduct"
+                    >
+                      Knight Hacks Code of Conduct
+                    </Link>
                     <Text className="text-[14px] leading-tight text-[#F7F0C6]">
                       Made with 💛 by the Knight Hacks team.
                     </Text>
@@ -200,6 +289,3 @@ export const KH8ConfirmationEmail = ({ name }: ConfirmationEmailProps) => {
 };
 
 export default KH8ConfirmationEmail;
-
-
-                
