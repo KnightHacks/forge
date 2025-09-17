a499b62c3a23f6e8d81d9f34cae2916be28ecf80
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2025-09-17 00:30:08 -0400

    Format fixes

--- packages/transactional/emails/knighthacks-viii/capacity-email.tsx
+++ packages/transactional/emails/knighthacks-viii/capacity-email.tsx
@@ -3,10 +3,10 @@ import {
   Container,
   Head,
   Html,
+  Link,
   Preview,
   Section,
   Tailwind,
-  Link,
   Text,
 } from "@react-email/components";
 
@@ -19,7 +19,7 @@ export const KH8CapacityEmail = ({ name }: ConfirmationEmailProps) => {
 
   return (
     <Html>
-      <Head/>
+      <Head />
       <Tailwind
         config={{
           theme: {
@@ -31,11 +31,13 @@ export const KH8CapacityEmail = ({ name }: ConfirmationEmailProps) => {
           },
         }}
       >
-        <Body className="bg-white m-0 p-0 font-sans"
-        style={{backgroundColor: "#ffffff"}}>
+        <Body
+          className="m-0 bg-white p-0 font-sans"
+          style={{ backgroundColor: "#ffffff" }}
+        >
           <Preview>{previewText}</Preview>
 
-          <Container className=" mx-auto max-w-[700px] p-0 bg-[#F7F0C6]">
+          <Container className="mx-auto max-w-[700px] bg-[#F7F0C6] p-0">
             <Section className="p-0">
               <table
                 role="presentation"
@@ -46,18 +48,18 @@ export const KH8CapacityEmail = ({ name }: ConfirmationEmailProps) => {
                 bgcolor="#F7F0C6"
               >
                 <tr>
-                  <td 
+                  <td
                     // @ts-expect-error td is tripping
                     background="https://i.imgur.com/OdRQ4Xy.jpeg"
-                    align="right" 
+                    align="right"
                     width="700"
                     height="180"
                     valign="top"
                     style={{
-                      backgroundSize: "700px 365px"
+                      backgroundSize: "700px 365px",
                     }}
-                    className="rounded-t-md relative"
-                    />
+                    className="relative rounded-t-md"
+                  />
                 </tr>
               </table>
             </Section>
@@ -73,18 +75,38 @@ export const KH8CapacityEmail = ({ name }: ConfirmationEmailProps) => {
               >
                 <tr>
                   <td className="px-4">
-                      <Text className="text-[20px] leading-tight text-black">
-                        <span className="font-bold text-[24px] text-[#C04B3D]">{name},</span><br/><br/>
-                        Unfortunately, we have finalized our list of attendees, and have to make the difficult decision to <span className="text-[#C04B3D] font-bold">deny you from Knight Hacks VIII. </span> 
-                        We have hit capacity for our venue, and regret that we could not find room for you.
-                        <br/><br/>
-                        Keep an eye on our <Link href={"https://discord.com/invite/Kv5g9vf"} className="text-[#4075b7] underline">
-                          Discord
-                        </Link> for news about our next hackathon, or others in the region. We would love for you to apply again next time!
-                        <br/><br/>
-                        Thanks for your interest,<br/>
-                        <span className="font-bold text-[#4075b7]">The Knight Hacks Team</span>
-                      </Text>
+                    <Text className="text-[20px] leading-tight text-black">
+                      <span className="text-[24px] font-bold text-[#C04B3D]">
+                        {name},
+                      </span>
+                      <br />
+                      <br />
+                      Unfortunately, we have finalized our list of attendees,
+                      and have to make the difficult decision to{" "}
+                      <span className="font-bold text-[#C04B3D]">
+                        deny you from Knight Hacks VIII.{" "}
+                      </span>
+                      We have hit capacity for our venue, and regret that we
+                      could not find room for you.
+                      <br />
+                      <br />
+                      Keep an eye on our{" "}
+                      <Link
+                        href={"https://discord.com/invite/Kv5g9vf"}
+                        className="text-[#4075b7] underline"
+                      >
+                        Discord
+                      </Link>{" "}
+                      for news about our next hackathon, or others in the
+                      region. We would love for you to apply again next time!
+                      <br />
+                      <br />
+                      Thanks for your interest,
+                      <br />
+                      <span className="font-bold text-[#4075b7]">
+                        The Knight Hacks Team
+                      </span>
+                    </Text>
                   </td>
                 </tr>
               </table>
@@ -101,12 +123,42 @@ export const KH8CapacityEmail = ({ name }: ConfirmationEmailProps) => {
               >
                 <tr>
                   <td className="px-4 text-center">
-                    <Link className="text-[#F7F0C6] underline mr-2" href="https://discord.com/invite/Kv5g9vf">Discord</Link>
-                    <Link className="text-[#F7F0C6] underline mr-2" href="https://www.instagram.com/knighthacks/">Instagram</Link>
-                    <Link className="text-[#F7F0C6] underline mr-2" href="https://blade.knighthacks.org/dashboard">Blade</Link>
-                    <Link className="text-[#F7F0C6] underline mr-2" href="https://knight-hacks.notion.site/knight-hacks-viii">Hacker's Guide</Link>
-                    <Link className="text-[#F7F0C6] underline mr-2" href="https://mlh.io/code-of-conduct">MLH Code of Conduct</Link>
-                    <Link className="text-[#F7F0C6] underline mr-2" href="https://knight-hacks.notion.site/code-of-conduct">Knight Hacks Code of Conduct</Link>
+                    <Link
+                      className="mr-2 text-[#F7F0C6] underline"
+                      href="https://discord.com/invite/Kv5g9vf"
+                    >
+                      Discord
+                    </Link>
+                    <Link
+                      className="mr-2 text-[#F7F0C6] underline"
+                      href="https://www.instagram.com/knighthacks/"
+                    >
+                      Instagram
+                    </Link>
+                    <Link
+                      className="mr-2 text-[#F7F0C6] underline"
+                      href="https://blade.knighthacks.org/dashboard"
+                    >
+                      Blade
+                    </Link>
+                    <Link
+                      className="mr-2 text-[#F7F0C6] underline"
+                      href="https://knight-hacks.notion.site/knight-hacks-viii"
+                    >
+                      Hacker's Guide
+                    </Link>
+                    <Link
+                      className="mr-2 text-[#F7F0C6] underline"
+                      href="https://mlh.io/code-of-conduct"
+                    >
+                      MLH Code of Conduct
+                    </Link>
+                    <Link
+                      className="mr-2 text-[#F7F0C6] underline"
+                      href="https://knight-hacks.notion.site/code-of-conduct"
+                    >
+                      Knight Hacks Code of Conduct
+                    </Link>
                     <Text className="text-[14px] leading-tight text-[#F7F0C6]">
                       Made with 💛 by the Knight Hacks team.
                     </Text>
@@ -122,6 +174,3 @@ export const KH8CapacityEmail = ({ name }: ConfirmationEmailProps) => {
 };
 
 export default KH8CapacityEmail;
-
-
-                
