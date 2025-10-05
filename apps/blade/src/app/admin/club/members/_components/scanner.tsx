c903e8f4e6a051cc378db983adb9de466b30db63
Arthur Teng <arthurteng0819@gmail.com>
2025-10-04 21:37:11 -0400

    delted fastpass column, moved discord role id to utils.ts, logic in hacker.ts looks at if hacker has vip role on discord

--- apps/blade/src/app/admin/club/members/_components/scanner.tsx
+++ apps/blade/src/app/admin/club/members/_components/scanner.tsx
@@ -77,7 +77,7 @@ const ScannerPopUp = ({ eventType }: { eventType: "Member" | "Hacker" }) => {
   });
   const hackerEventCheckIn = api.hacker.eventCheckIn.useMutation({
     onSuccess(opts) {
-      toast.success(opts.message);
+      toast.success(opts.eventName);
       setFirstName(opts.firstName);
       setLastName(opts.lastName);
       setAssignedClass(opts.class ?? "No class assigned");
