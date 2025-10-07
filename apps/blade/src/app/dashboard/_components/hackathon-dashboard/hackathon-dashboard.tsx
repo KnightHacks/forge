00652cb3c1e8db33ad2e85a809e7782d11a69cb2
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2025-10-07 11:21:44 -0400

    Incomplete interface

---
+++ apps/blade/src/app/dashboard/_components/hackathon-dashboard/hackathon-dashboard.tsx
@@ -0,0 +1,82 @@
+import type { Metadata } from "next";
+
+import type { api as serverCall } from "~/trpc/server";
+import { HackerAppCard } from "~/app/_components/option-cards";
+import { api } from "~/trpc/server";
+import { HackathonData } from "./hackathon-data";
+
+export const metadata: Metadata = {
+  title: "Hacker Dashboard",
+  description: "The official Knight Hacks Hacker Dashboard",
+};
+
+export default async function HackathonDashboard({
+  hacker,
+}: {
+  hacker: Awaited<ReturnType<(typeof serverCall.hacker)["getHacker"]>>;
+}) {
+  //   const [resume, pastHackathons] = await Promise.allSettled([
+  //     api.resume.getResume(),
+  //     api.hackathon.getPastHackathons(),
+  //   ]);
+
+  const currentHackathon = await api.hackathon.getCurrentHackathon();
+
+  if (!hacker) {
+    return (
+      <div className="flex flex-col items-center justify-center gap-y-6 text-xl font-semibold">
+        <p className="w-full max-w-xl text-center text-2xl">
+          Register for KnightHacks today!
+        </p>
+        <div className="flex flex-wrap justify-center gap-5">
+          {
+            //if there is no current hackathon then this page is never rendered anyway
+            currentHackathon && (
+              <HackerAppCard hackathonName={currentHackathon.name} />
+            )
+          }
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <div className="animate-fade-in mb-8">
+        <h2 className="text-xl font-bold tracking-tight">
+          Hello, {hacker.firstName}!
+        </h2>
+        <p className="text-muted-foreground">Hackathon Dashboard</p>
+      </div>
+      <div className="animate-mobile-initial-expand relative mx-auto flex h-0 bg-[#E5E7EB] dark:bg-[#0A0F1D] sm:py-0 sm:pb-0 lg:max-h-56">
+        {/* Main content */}
+        <HackathonData data={hacker} />
+
+        {/* Transparent Triangle overlay in bottom right corner */}
+        <div className="border-b-solid border-l-solid absolute bottom-0 right-0 h-0 w-0 border-b-[30px] border-l-[30px] border-b-background border-l-transparent"></div>
+
+        {/* Triangle in bottom right corner */}
+        <div
+          className="absolute bottom-0 right-0 h-0 w-0"
+          style={{
+            borderBottom: "20px solid #6C26D9", // Change to bg color
+            borderLeft: "20px solid transparent",
+          }}
+        ></div>
+
+        {/* Top rectangle */}
+        <div className="absolute -top-[1.4rem] right-0 h-6 w-40 bg-[#E5E7EB] dark:bg-[#0A0F1D] sm:w-96">
+          <div className="border-t-solid border-r-solid absolute left-0 top-0 h-0 w-0 border-r-[23px] border-t-[23px] border-r-transparent border-t-background"></div>
+        </div>
+
+        {/* Bottom rectangle */}
+        <div className="absolute -bottom-[1.46rem] left-0 h-6 w-40 bg-[#E5E7EB] dark:bg-[#0A0F1D] sm:w-48">
+          <div className="border-b-solid border-l-solid absolute bottom-0 right-0 h-0 w-0 border-b-[24px] border-l-[24px] border-b-background border-l-transparent"></div>
+        </div>
+
+        {/* Left side rectangle */}
+        <div className="absolute -left-3 top-0 h-full w-[0.4rem] bg-primary"></div>
+      </div>
+    </>
+  );
+}
