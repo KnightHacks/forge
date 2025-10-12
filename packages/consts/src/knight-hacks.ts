03ac8fe5195f2056f2814cc1d85fc2b4478b9785
unknown <danielram2023@gmail.com>
2025-10-12 00:30:15 -0400

    add permissions const

--- packages/consts/src/knight-hacks.ts
+++ packages/consts/src/knight-hacks.ts
@@ -262,6 +262,15 @@ export const PERMISSIONS = {
 export type PermissionKey = keyof typeof PERMISSIONS;
 export type PermissionIndex = (typeof PERMISSIONS)[PermissionKey];
 
+export const PERMISSIONS = {
+  FULL_ADMIN: 0,
+  CHECK_IN: 1,
+  // Future permissions will be added here with incremental indices
+  // EVENTS_MANAGE: 2,
+  // MEMBERS_MANAGE: 3,
+  // etc.
+} as const;
+
 export const PROD_KNIGHTHACKS_GUILD_ID = "486628710443778071";
 export const DEV_KNIGHTHACKS_GUILD_ID = "1151877367434850364";
 
