1ec37196ec71709620bab53d611795c8b93bdb7b
unknown <danielram2023@gmail.com>
2025-10-12 00:53:10 -0400

    update auth router with the new permission endpoints

--- packages/api/src/routers/auth.ts
+++ packages/api/src/routers/auth.ts
@@ -1,9 +1,11 @@
 import type { TRPCRouterRecord } from "@trpc/server";
+import { z } from "zod";
 
 import { invalidateSessionToken } from "@forge/auth";
+import { PERMISSIONS } from "@forge/consts/knight-hacks";
 
 import { protectedProcedure, publicProcedure } from "../trpc";
-import { isDiscordAdmin, isDiscordMember } from "../utils";
+import { isDiscordAdmin, isDiscordMember, getUserPermissions, userHasPermission, userHasFullAdmin, userHasCheckIn} from "../utils";
 
 export const authRouter = {
   getSession: publicProcedure.query(({ ctx }) => {
@@ -25,6 +27,38 @@ export const authRouter = {
     }
     return isDiscordMember(ctx.session.user);
   }),
+
+  getUserPermissions: publicProcedure.query(({ ctx }): Promise<string> => {
+    if (!ctx.session) {
+      return Promise.resolve("00");
+    }
+    return getUserPermissions(ctx.session.user);
+  }),
+
+  hasPermission: publicProcedure
+    .input(z.object({ permission: z.number() }))
+    .query(({ ctx, input }): Promise<boolean> => {
+      if (!ctx.session) {
+        return Promise.resolve(false);
+      }
+      return userHasPermission(ctx.session.user, input.permission);
+    }),
+
+  hasFullAdmin: publicProcedure.query(({ ctx }): Promise<boolean> => {
+    if (!ctx.session) {
+      return Promise.resolve(false);
+    }
+    return userHasFullAdmin(ctx.session.user);
+  }),
+
+  hasCheckIn: publicProcedure.query(({ ctx }): Promise<boolean> => {
+    if (!ctx.session) {
+      return Promise.resolve(false);
+    }
+    return userHasCheckIn(ctx.session.user);
+  }),
+
+
   signOut: protectedProcedure.mutation(async (opts) => {
     if (!opts.ctx.token) {
       return { success: false };
