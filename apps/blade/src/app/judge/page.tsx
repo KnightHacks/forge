b32f1a525fc925c756a471f568709f0f00cbb69a
FernandoAilon <fernandoailon26@yahoo.com>
2025-10-12 18:51:29 -0400

    Judge dashboard

--- apps/blade/src/app/judge/page.tsx
+++ apps/blade/src/app/judge/page.tsx
@@ -1,3 +1,25 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@forge/ui/card";
+import { FilterBar } from "./_components/filter-bar";
+import { SearchBar } from "./_components/search-bar";
+import { ProjectsTable } from "./_components/projects-table";
+
+
 export default function Page() {
-  return <div>Judging</div>;
+  return (
+    <div className="container h-screen py-16">
+      <div className="flex flex-col justify-center gap-6">
+        <FilterBar />
+        <SearchBar />
+
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-center">Judge Dashboard</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <ProjectsTable />
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
