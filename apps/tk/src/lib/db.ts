ba43065a63b8d8b551b0f3e1e7e61cc690df8a0d
Sam <samuelxborges@gmail.com>
2025-10-13 16:15:35 -0400

    email queue sorta kinda

---
+++ apps/tk/src/lib/db.ts
@@ -0,0 +1,13 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+import { env } from "../env";
+
+// Create the connection
+const connectionString = env.DATABASE_URL;
+const client = postgres(connectionString, {
+  onnotice: () => {}, // Suppress notices
+});
+
+// Create the database instance
+export const db = drizzle(client);
