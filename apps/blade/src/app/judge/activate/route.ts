cb924fc65e32fd40e36e7cfe1892fad4c1447578
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2025-10-24 11:11:52 -0400

    fix redirect url

--- apps/blade/src/app/judge/activate/route.ts
+++ apps/blade/src/app/judge/activate/route.ts
@@ -1,6 +1,7 @@
 import type { TRPCError } from "@trpc/server";
 import { NextResponse } from "next/server";
 
+import { env } from "~/env";
 import { api } from "~/trpc/server";
 
 export async function GET(req: Request) {
@@ -12,7 +13,7 @@ export async function GET(req: Request) {
 
   try {
     await api.judge.activateToken({ token });
-    return NextResponse.redirect(new URL("/judge", req.url));
+    return NextResponse.redirect(`${env.BLADE_URL}/judge`);
   } catch (err) {
     return NextResponse.json(
       { error: (err as TRPCError).message },
