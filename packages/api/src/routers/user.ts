4a3dcc65bca3b4b2c42af62b2a32f7398cc64d7c
Arch <rhygonfn@gmail.com>
2025-11-04 20:40:27 -0500

    Readded correct avatar usage

--- packages/api/src/routers/user.ts
+++ packages/api/src/routers/user.ts
@@ -4,7 +4,14 @@ import { protectedProcedure } from "../trpc";
 
 export const userRouter = {
   getUserAvatar: protectedProcedure.query(({ ctx }) => {
-    const avatarUrl = ctx.session.user.image;
+    const discordId = ctx.session.user.discordUserId;
+    const avatarHash = ctx.session.user.image;
+    let avatarUrl = "";
+    if (avatarHash) {
+      // User has a custom avatar
+      const isAnimated = avatarHash.startsWith("a_");
+      avatarUrl = `https://cdn.discordapp.com/avatars/${discordId}/${avatarHash}.${isAnimated ? "gif" : "png"}`;
+    }
     return { avatar: avatarUrl, name: ctx.session.user.name };
   }),
 } satisfies TRPCRouterRecord;
