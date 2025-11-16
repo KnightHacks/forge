92943de7a5084023bdbeadb09d6ff72c2521f3a3
Arch <rhygonfn@gmail.com>
2025-11-15 20:29:23 -0500

    Some alumni fluff

---
+++ apps/blade/src/app/dashboard/_components/member-dashboard/AlumniDiscord.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { MessageCircle, MessageSquare } from "lucide-react";
+
+import { Button } from "@forge/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@forge/ui/card";
+
+import { DASHBOARD_ICON_SIZE } from "~/consts";
+
+export function AlumniDiscord() {
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-medium">Alumni Discord</CardTitle>
+        <MessageCircle
+          color="hsl(263.4 70% 50.4%)"
+          size={DASHBOARD_ICON_SIZE}
+        />
+      </CardHeader>
+      <CardContent>
+        <p className="mb-2 text-left text-[12px]">
+          Chat in the exlusive Alumni-only Knight Hacks Discord channel!
+        </p>
+        <Button className="w-full bg-[#5865F2]" asChild>
+          <Link href={"WOWZA"}>
+            <MessageSquare size={DASHBOARD_ICON_SIZE} className="mr-1" />
+            Discord Channel
+          </Link>
+        </Button>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ apps/blade/src/app/dashboard/_components/member-dashboard/payment/donate.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import { CreditCard, Heart } from "lucide-react";
+
+import type { InsertMember } from "@forge/db/schemas/knight-hacks";
+import { Button } from "@forge/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@forge/ui/card";
+
+import { DASHBOARD_ICON_SIZE } from "~/consts";
+import PaymentButton from "./payment-button";
+
+export function Donate() {
+  const [hover, setHover] = useState(false);
+  return (
+    <Card>
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-medium">Donate</CardTitle>
+        <CreditCard color="hsl(263.4 70% 50.4%)" size={DASHBOARD_ICON_SIZE} />
+      </CardHeader>
+      <CardContent className="h-full">
+        <div className="mt-3 h-full flex-col align-middle">
+          <p className="mb-2 text-left text-[12px]">
+            Empower the next generation of hackers
+          </p>
+          <Button
+            className="w-full"
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            asChild
+          >
+            <Link href={"WOWZA"}>
+              <Heart
+                size={DASHBOARD_ICON_SIZE}
+                fill={hover ? "white" : "none"}
+                className="mr-1"
+              />
+              Donate to Knight Hacks
+            </Link>
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
