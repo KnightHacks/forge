ec0eb8100db22ffcdafd888a8b90f9fa9932dcfd
Arch <rhygonfn@gmail.com>
2025-11-24 15:20:36 -0500

    Converted to json schema creation, abstracted into forms.ts

---
+++ packages/api/src/routers/forms.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+import { db } from "@forge/db/client";
+
+import { publicProcedure } from "../trpc";
+
+export const challengeRouter = {
+  getChallenges: publicProcedure
+    .input(z.object({ hackathonId: z.string() }))
+    .query(async ({ input }) => {
+      const challenges = db.query.Challenges.findMany({
+        where: (t, { eq }) => eq(t.hackathonId, input.hackathonId),
+      });
+      return challenges;
+    }),
+};
