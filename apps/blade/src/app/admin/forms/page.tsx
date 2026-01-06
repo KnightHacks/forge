07da3ba84fbc7891deee8fc6f0aa83e9144a377f
Bryan <bryanefetaylan@gmail.com>
2026-01-06 12:45:42 -0500

    Add authentication to forms edit page

--- apps/blade/src/app/admin/forms/page.tsx
+++ apps/blade/src/app/admin/forms/page.tsx
@@ -1,9 +1,23 @@
+import { auth } from "@forge/auth";
 import FormsClient from "../../_components/admin/forms/homepage";
+import { SIGN_IN_PATH } from "~/consts";
+import { redirect } from "next/navigation";
+import { api } from "~/trpc/server";
 
 export const metadata = {
   title: "Forms",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  if (!session) {
+    redirect(SIGN_IN_PATH);
+  }
+
+  const isAdmin = await api.auth.getAdminStatus();
+  if (!isAdmin) {
+    redirect("/");
+  }
+
   return <FormsClient />;
 }
