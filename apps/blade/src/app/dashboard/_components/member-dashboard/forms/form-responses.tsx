38fceebddc9a6865e36dfc5f29a7e4fba2c721e0
Aiden Letourneau <aidenletourneau@gmail.com>
2026-01-12 11:04:11 -0500

    added form submission to member dash

---
+++ apps/blade/src/app/dashboard/_components/member-dashboard/forms/form-responses.tsx
@@ -0,0 +1,70 @@
+import Link from "next/link";
+import { Info } from "lucide-react";
+
+import { Button } from "@forge/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@forge/ui/card";
+
+import { DASHBOARD_ICON_SIZE } from "~/consts";
+import { api } from "~/trpc/server";
+
+export async function FormResponses() {
+  const userFormResponeses = await api.forms.getUserResponse({});
+
+  const hasResponses = userFormResponeses.length > 0;
+
+  return (
+    <Card className="h-full">
+      <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-sm font-medium">Form Submissions</CardTitle>
+        <Info color="hsl(263.4 70% 50.4%)" size={DASHBOARD_ICON_SIZE} />
+      </CardHeader>
+
+      {hasResponses ? (
+        <CardContent className="flex-1">
+          <div className="flex h-36 flex-col gap-3 overflow-y-auto">
+            {userFormResponeses.map((formResponse) => (
+              <div
+                key={`view-response-button-${formResponse.id}`}
+                className="rounded-lg border bg-muted/50 p-3 text-sm"
+              >
+                <div className="flex items-center justify-between">
+                  <div className="flex flex-col">
+                    <span className="font-medium">{formResponse.formName}</span>
+                    <span className="text-xs text-muted-foreground">
+                      {new Date(formResponse.submittedAt).toLocaleString()}
+                    </span>
+                  </div>
+
+                  <ViewFormResponseButton
+                    formNameSlug={formResponse.formSlug ?? ""}
+                    responseIdSlug={formResponse.id ?? ""}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </CardContent>
+      ) : (
+        <CardHeader>
+          <CardTitle>
+            <div className="text-sm text-gray-600">No form submissions yet</div>
+          </CardTitle>
+        </CardHeader>
+      )}
+    </Card>
+  );
+}
+
+function ViewFormResponseButton({
+  responseIdSlug,
+  formNameSlug,
+}: {
+  responseIdSlug: string;
+  formNameSlug: string;
+}) {
+  return (
+    <Link href={`/forms/${formNameSlug}/${responseIdSlug}`}>
+      <Button size="sm">View/Edit</Button>
+    </Link>
+  );
+}
