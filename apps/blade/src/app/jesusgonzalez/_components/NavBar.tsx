dca0fe3c4524403b5ebf2c01d5b6a37c8f017d2b
Jesus Gonzalez <reychucho859@gmail.com>
2026-01-21 16:38:00 -0500

    Added Tooltip to Navbar

--- apps/blade/src/app/jesusgonzalez/_components/NavBar.tsx
+++ apps/blade/src/app/jesusgonzalez/_components/NavBar.tsx
@@ -1,12 +1,27 @@
 'use client'
 import React from 'react';
 import { CodeIcon } from '@forge/ui'
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@forge/ui/tooltip"
 
 const NavBar = () => {  
   return (  
     <nav className='sticky top-0 z-50 p-4 flex items-center justify-center bg-white/0 rounded-2xl shadow-[0_4px_30px_rgba(0,0,0,0.1)] backdrop-blur-[4.4px]'>
       <span className='text-xl font-bold relative z-10'>
-        <CodeIcon className='h-7 w-7 hover:scale-110 transition' />
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <CodeIcon className='h-6 w-6 hover:scale-110 transition' />
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>I Do Nothing :D</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </span>
     </nav>
   );
