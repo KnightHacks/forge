eeeb590f7b8bf120c05a537bf04facdbfd00d82b
Dylan Vidal <dylanvidal1204@gmail.com>
2026-01-26 17:05:07 -0500

    Update next.config.js

--- apps/guild/next.config.js
+++ apps/guild/next.config.js
@@ -22,6 +22,10 @@ const config = {
         protocol: "https",
         hostname: "minio-g0soogg4gs8gwcggw4ococok.knighthacks.org",
       },
+      {
+        protocol: "https",
+        hostname: "minio-y44gsgsskc4ko4kkwsg0csoc.135.237.97.107.sslip.io",
+      },
     ],
   },
 };
