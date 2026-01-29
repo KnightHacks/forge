9fd663b47501e16c682683a5453e3c52af240bc2
DGoel1602 <rhygonfn@gmail.com>
2026-01-29 13:39:17 -0500

    fix: format...

--- packages/db/scripts/seed_devdb.ts
+++ packages/db/scripts/seed_devdb.ts
@@ -13,6 +13,7 @@ import { Routes } from "discord-api-types/v10";
 import { sql } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/node-postgres";
 import Pool from "pg-pool";
+import { stringify } from "superjson";
 
 import {
   DEV_KNIGHTHACKS_GUILD_ID,
@@ -25,7 +26,6 @@ import { discord, log } from "../../api/src/utils";
 import { env } from "../src/env";
 import * as authSchema from "../src/schemas/auth";
 import * as knightHacksSchema from "../src/schemas/knight-hacks";
-import { stringify } from "superjson";
 
 const execAsync = promisify(exec);
 console.log("Starting seeding script");
@@ -419,22 +419,22 @@ async function main() {
     console.log("Cleaning up backup db");
     await cleanUp();
 
-		await log({
-			title: `Successfully saved limited prod db to minio`,
-			message: `Successfully saved limited prod db to minio. Run the get_prod_db.ts script to get it into your local dev db.`,
-			color: "success_green",
-			userId: "Host",
-		});
+    await log({
+      title: `Successfully saved limited prod db to minio`,
+      message: `Successfully saved limited prod db to minio. Run the get_prod_db.ts script to get it into your local dev db.`,
+      color: "success_green",
+      userId: "Host",
+    });
 
     process.exit(0);
   } catch (error) {
     console.error("Error during database seeding:", error);
-		await log({
-			title: `Failed to save limited prod db to minio`,
-			message: `Failed to sav limited prod db to minio. Error: ${stringify(error)}`,
-			color: "uhoh_red",
-			userId: "Host",
-		});
+    await log({
+      title: `Failed to save limited prod db to minio`,
+      message: `Failed to sav limited prod db to minio. Error: ${stringify(error)}`,
+      color: "uhoh_red",
+      userId: "Host",
+    });
     await cleanUp();
     process.exit(1);
   }
