e95d2b011d0f8aa837e370b2727ef3970d7384f7
Schevis <daniel.schevis@gmail.com>
2026-01-30 19:47:35 -0500

    added SUPER basic page to get started and placeholders with .env

---
+++ apps/blade/src/app/daniel-schevis/page.tsx
@@ -0,0 +1,9 @@
+export default function DanielSchevisPortfolio() {
+  return (
+    <div className="min-h-screen w-full bg-white">
+      <h1 className="p-8 text-4xl font-bold text-black">
+        Daniel Schevis Portfolio
+      </h1>
+    </div>
+  );
+}
