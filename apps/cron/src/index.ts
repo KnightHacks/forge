202fd5b8d0f806c2e9670dd53399ceb5e75fad85
Alexander Paolini <30964205+alexanderpaolini@users.noreply.github.com>
2026-02-05 23:32:21 -0500

    feat(cron/reminder): move over reminder

--- apps/cron/src/index.ts
+++ apps/cron/src/index.ts
@@ -1,15 +1,23 @@
 import { alumniAssign } from "./crons/alumni-assign";
 import { capybara, cat, duck, goat } from "./crons/animals";
 import { backupFilteredDb } from "./crons/backup-filtered-db";
+import { leetcode } from "./crons/leetcode";
+import { hackReminders, preReminders, reminders } from "./crons/reminder";
 import { roleSync } from "./crons/role-sync";
+
+alumniAssign.schedule();
 
 cat.schedule();
 capybara.schedule();
 duck.schedule();
 goat.schedule();
 
-roleSync.schedule();
-
-alumniAssign.schedule();
-
 backupFilteredDb.schedule();
+
+leetcode.schedule();
+
+preReminders.schedule();
+reminders.schedule();
+hackReminders.schedule();
+
+roleSync.schedule();
