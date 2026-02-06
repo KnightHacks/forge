c6bf2cf59ffb3e3d00ece20d09e42f7932d8550a
Alexander Paolini <30964205+alexanderpaolini@users.noreply.github.com>
2026-02-05 19:01:58 -0500

    feat(cron/role-sync): move over role-sync to new cron app
    Co-Authored-By: Dhruv Goel <98714172+DGoel1602@users.noreply.github.com>
    Co-Authored-By: brianush1 <brianush1@outlook.com>

--- apps/cron/src/index.ts
+++ apps/cron/src/index.ts
@@ -1,6 +1,9 @@
 import { capybara, cat, duck, goat } from "./crons/animals";
+import { roleSync } from "./crons/role-sync";
 
 cat.schedule();
 capybara.schedule();
 duck.schedule();
 goat.schedule();
+
+roleSync.schedule();
