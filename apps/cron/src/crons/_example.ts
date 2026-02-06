8d6dadd4e45c530331b47d8e9b528c2292b68088
Alexander Paolini <30964205+alexanderpaolini@users.noreply.github.com>
2026-02-05 22:25:34 -0500

    chore(cron): fix explanation comments

--- apps/cron/src/crons/_example.ts
+++ apps/cron/src/crons/_example.ts
@@ -2,7 +2,7 @@ import { CronBuilder } from "../structs/CronBuilder";
 
 export const testCron = new CronBuilder({
   name: "test",
-  cronExpression: "*/1 * * * * ",
+  cronExpression: "* * * * * ", // every minute
   color: 1,
 }).addExecutor(() => {
   console.log("This is an example cron that runs every minute");
