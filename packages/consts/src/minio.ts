19a8db68c8924bcf9b9acbe1577183ba2fb3170c
Alexander Paolini <30964205+alexanderpaolini@users.noreply.github.com>
2026-02-08 18:06:28 -0500

    chore(consts): add bucket names to minio.ts

--- packages/consts/src/minio.ts
+++ packages/consts/src/minio.ts
@@ -1,14 +1,17 @@
 export const ENDPOINT = "minio-g0soogg4gs8gwcggw4ococok.knighthacks.org";
 
+export const BUCKET_REGION = "us-east-1";
+
 export const QR_CONTENT_TYPE = "image/png";
 export const QR_PATHNAME = "/knight-hacks-qr/**";
+export const QR_BUCKET_NAME = "knight-hacks-qr";
 
-export const BUCKET_NAME = "knight-hacks-qr";
-export const BUCKET_REGION = "us-east-1";
+export const PROFILE_PICTURES_BUCKET_NAME = "guild-profile-pictures";
+export const FORM_ASSETS_BUCKET_NAME = "form-assets";
+
 // TODO: check if this should be MB or MiB
 export const MAX_RESUME_SIZE = 5 * 1000000; // 5MB
 
-export const FORM_ASSETS_BUCKET = "form-assets";
 
 export const PRESIGNED_URL_EXPIRY = 7 * 24 * 60 * 60; // 7 days
 
