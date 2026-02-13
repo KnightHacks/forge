d294c12fbfc2e8a30097493098d3a46faeb10652
Fernando <crespofernando05@gmail.com>
2026-02-12 23:38:39 -0500

    removal of more metadata

--- apps/blade/src/app/judge/results/page.tsx
+++ apps/blade/src/app/judge/results/page.tsx
@@ -1,13 +1,7 @@
-import type { Metadata } from "next";
 import { redirect } from "next/navigation";
 
 import { api, HydrateClient } from "~/trpc/server";
 import ResultsTable from "./_components/results-table";
-
-export const metadata: Metadata = {
-  title: "Blade | Hackathon Results",
-  description: "Display hackathon results",
-};
 
 export default async function ResultsDashboard() {
   const hasAccess = await api.roles.hasPermission({
