52e70cc655a9370d8871996ebf4cf64bf9d7ca61
luciano093 <luparedes100@gmail.com>
2026-03-10 19:19:24 -0400

    Add working logic to deleteTemplate and getTemplates

--- packages/api/src/routers/templates.ts
+++ packages/api/src/routers/templates.ts
@@ -1,6 +1,12 @@
 import type { TRPCRouterRecord } from "@trpc/server";
-import { permProcedure } from "../trpc";
+import z from "zod";
+
+import { eq } from "@forge/db";
+import { db } from "@forge/db/client";
+import { Template } from "@forge/db/schemas/knight-hacks";
 import { permissions } from "@forge/utils";
+
+import { permProcedure } from "../trpc";
 
 export const templatesRouter = {
   createTemplate: permProcedure.mutation(({ ctx }) => {
@@ -13,14 +19,24 @@ export const templatesRouter = {
 
     return null;
   }),
-  deleteTemplate: permProcedure.mutation(({ ctx }) => {
-    permissions.controlPerms.or(["EDIT_ISSUE_TEMPLATES"], ctx);
+  deleteTemplate: permProcedure
+    .input(
+      z.object({
+        id: z.string().uuid(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      permissions.controlPerms.or(["EDIT_ISSUE_TEMPLATES"], ctx);
 
-    return null;
-  }),
-  getTemplates: permProcedure.query(({ ctx }) => {
+      await db.delete(Template).where(eq(Template.id, input.id));
+    }),
+  getTemplates: permProcedure.query(async ({ ctx }) => {
     permissions.controlPerms.or(["READ_ISSUE_TEMPLATES"], ctx);
 
-    return null;
+    const templates = await db.query.Template.findMany({
+      orderBy: (templates, { desc }) => [desc(templates.createdAt)],
+    });
+
+    return templates;
   }),
 } satisfies TRPCRouterRecord;
