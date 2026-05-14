c4ca25350f9b43301509616f4d776c87039789ac
Faris Abdelrahem <farishabdelrahem@gmail.com>
2026-05-14 00:37:53 -0400

    implement hero component for club site sponsorship page
    changes:
        - create hero component which matches the figma
        - add light variation of club-button css in global.css
        - add 900 font weight import for inter font in global.css
    
    known issues:
        - no dynamic sizing for mobile/smaller screens
        - navbar has no padding which causes visual bugs on small screens
        - button is not exact, needs changes to dropshadow and size
        - hero middle textbox may be altered for a stronger design
    
    out of scope issues:
        - some files in src do not follow the prettier format
        - linter warnings outside of hero section
        - cannot build site, difficult to trace error
    
    for that last one, with a grain of salt, a quick prompt led to the issue
    possibly being with the navbar array and how that's set up, but i'd
    reccomend an actual look into it when it comes time to fix it

--- apps/club/src/app/_components/sponsor/hero.tsx
+++ apps/club/src/app/_components/sponsor/hero.tsx
@@ -1,21 +1,24 @@
+import { Button } from "@forge/ui/button";
+
 const Hero = () => {
   return (
-    <div className="min-h-screen -translate-y-10 flex flex-col justify-center items-center">
-      <h1>Our Supporters</h1>
-      <h3>Sponsors & Partners</h3>
+    <div className="font-inter uppercase min-h-screen flex flex-col justify-center items-center">
+      <h1 className="font-black text-[64px]">Our Supporters</h1>
+      <h3 className="text-[24px] text-(--club-gold)">Sponsors & Partners</h3>
 
-      <div className="relative border-4 border-white mx-20 mt-14 mb-17 py-11 px-16">
+      <div className="-rotate-1 relative border-4 border-white max-w-4xl mx-20 mt-14 mb-17 py-11 px-16">
         <div className="absolute top-2.5 left-2.5 border-t-4 border-l-4 border-white w-8 h-8 ">
         </div>
-        <p>Our sponsors make it possible for us to unite developers, designers, and builders across Florida and beyond. Hackers form meaningful connections with our sponsors that last well past the hackathon weekend.</p>
+        <p className="text-[20px]">Our sponsors make it possible for us to unite developers, designers, and builders across Florida and beyond. Hackers form meaningful connections with our sponsors that last well past the hackathon weekend.</p>
         <div className="absolute right-2.5 bottom-2.5 border-b-4 border-r-4 border-white w-8 h-8 ">
         </div>
 
       </div>
-      <button>Become a Sponsor</button>
+      <Button asChild className="min-w-57 h-17 club-button club-button-light font-black!">
+        <a href="https://blade.knighthacks.org/sponsor">Become a Sponsor →</a>
+      </Button>
     </div>
   )
 };
 
 export default Hero;
-
