d0eca39dd40bd924ea28720e3843ab783e9faba8
pinkytoefoo <agadrian1331@gmail.com>
2026-09-07 12:05:30 -0400

    dev page, icons, navbar

--- apps/blade/src/app/adrian-garced/page.tsx
+++ apps/blade/src/app/adrian-garced/page.tsx
@@ -1,13 +1,38 @@
 
-
-
+// import {
+//   Github,
+//   Code
+// } from "lucide-react";
+import {
+  GithubIcon,
+  CodebergIcon
+} from "./icons"
 
 export default async function AdrianG() {
-
+  
 
   return (
-    <main>
-      <p>wasssaaa</p>
+    <main className="flex justify-center">
+      <nav className="w-full border-b">
+        <div className="mx-auto flex max-w-4xl items-center justify-between px-6 py-4">
+          <a href="#" className="font-semibold">
+            adrian-g
+          </a>
+
+          <div className="flex items-center gap-4">
+            <a href="..." className="flex items-center gap-2">
+              <GithubIcon className="size-6" />
+              {/* GitHub */}
+            </a>
+
+            <a href="..." className="flex items-center gap-2">
+              <CodebergIcon />
+              {/* Codeberg */}
+            </a>
+          </div>
+        </div>
+      </nav>
+        <p></p>
     </main>
   );
 }
